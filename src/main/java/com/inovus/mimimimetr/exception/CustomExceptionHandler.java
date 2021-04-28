d69e4c2eb665540bf494212f8d3ea514b08ca2c1
m.maksiutov <m.maksiutov@bars.group>
2021-04-28 13:00:33 +0300

    refact

--- src/main/java/com/inovus/mimimimetr/exception/CustomExceptionHandler.java
+++ src/main/java/com/inovus/mimimimetr/exception/CustomExceptionHandler.java
@@ -7,9 +7,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class CustomExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public String handle(Model model, Exception e) {
+    @ExceptionHandler(CustomException.class)
+    public String handleCustom(Model model, CustomException e) {
         model.addAttribute("errorMessage", e.getMessage());
         return "index";
     }
+
+    @ExceptionHandler(Exception.class)
+    public String handleAll(Model model, Exception e) {
+        model.addAttribute("errorMessage", "Произошла ошибка, попробуйте еще раз");
+        return "index";
+    }
+
 }
