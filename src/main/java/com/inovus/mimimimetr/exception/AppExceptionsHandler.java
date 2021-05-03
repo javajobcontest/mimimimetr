13be2353ce382f098e8093ca787a64aa3b8d9be9
m.maksiutov <m.maksiutov@bars.group>
2021-05-03 13:19:26 +0300

    refactoring

--- src/main/java/com/inovus/mimimimetr/exception/AppExceptionsHandler.java
+++ src/main/java/com/inovus/mimimimetr/exception/AppExceptionsHandler.java
@@ -1,5 +1,6 @@
 package com.inovus.mimimimetr.exception;
 
+import com.inovus.mimimimetr.util.PagesEndpoints;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,13 +11,13 @@ public class AppExceptionsHandler {
     @ExceptionHandler(CustomException.class)
     public String handleCustom(Model model, CustomException e) {
         model.addAttribute("errorMessage", e.getMessage());
-        return "index";
+        return PagesEndpoints.INDEX_PAGE;
     }
 
     @ExceptionHandler(Exception.class)
     public String handleAll(Model model, Exception e) {
         model.addAttribute("errorMessage", "Возникла ошибка, попробуйте еще раз.");
         e.printStackTrace();
-        return "index";
+        return PagesEndpoints.INDEX_PAGE;
     }
 }
